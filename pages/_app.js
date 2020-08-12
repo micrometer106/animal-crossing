e4002a1f38950a760f27ea854ed17aad34925fe3
Ailing Tung <ailingtung@kktv.me>
2020-08-12 15:50:05 +0800

    add to _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import App from 'next/app';
 import MainLayout from '../components/layouts/main';
 import DefaultLayout from '../components/layouts/default';
 
+//test deve
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
